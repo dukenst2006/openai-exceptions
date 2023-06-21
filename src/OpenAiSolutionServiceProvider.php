5510af83e0aed23e52463478bfdbc59a226cbf5e
dukenst2006 <dukenst2006@gmail.com>
2023-06-21 17:17:03 -0400

    Clean up the boot method

--- src/OpenAiSolutionServiceProvider.php
+++ src/OpenAiSolutionServiceProvider.php
@@ -10,22 +10,9 @@ class OpenAiSolutionServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'openai-exceptions');
-
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/nanos/openai-exceptions'),
-        ], 'views');
-
-        $this->publishes([
-            __DIR__ . '/../config/openai-exceptions.php' => config_path('openai-exceptions.php'),
+            __DIR__ . '/../config/openai-exceptions.php' => $this->app->configPath('openai-exceptions.php'),
         ], 'config');
-
-        if (config('openai-exceptions.api_key') && !config('openai.api_key')) {
-            Config::set('openai.api_key', config('openai-exceptions.api_key'));
-        }
-        if (config('openai-exceptions.organization') && !config('openai.organization')) {
-            Config::set('openai.organization', config('openai-exceptions.organization'));
-        }
 
         if (config('openai-exceptions.api_key')) {
             $this->app->make(SolutionProviderRepository::class)
